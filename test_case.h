4cad22854552833d49da05dafd57e2ee19d38026
w <ghostoftheshell@mailbox.org>
2024-07-29 15:33:04 +0300

    Add engage function

--- test_case.h
+++ test_case.h
@@ -2,6 +2,7 @@
 #define TEST_CASE_H_
 
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 typedef enum {
@@ -11,12 +12,12 @@ typedef enum {
 
 struct TestContext;
 
-typedef TestResult(*TestCase) (struct TestContext) ;
+// TODO: test descriptions
+typedef TestResult(*TestCase) (struct TestContext*) ;
 
 typedef struct TestContext {
 	// TODO: init?
 	// TODO: arraylist?
-	// TODO: test descriptions
 	size_t testCount;
 	TestCase *tests;
 
@@ -33,6 +34,26 @@ tTC_AllocTestContext() {
 static inline void
 tTC_AddTest(TestContext *tc, TestCase tcase) {
 	tc->tests[tc->testCount++] = tcase;
+}
+
+static inline void
+tTC_Engage(TestContext *tc) {
+	// TODO: colorful printing 
+	size_t okCount = 0;
+	for (size_t i = 0; i < tc->testCount; i++) {
+		if (tc->tests[i](tc) == TEST_OK) {
+			printf("Test %d: OK\n");
+			okCount++;
+		} else {
+			printf("Test %d: Error\n");
+		}
+	}
+	
+	if (okCount == tc->testCount) {
+		printf("Tests passed %d/%d (all passed)\n");
+	} else {
+		printf("Tests passed %d/%d (errors)\n");
+	}
 }
 
 static inline void
