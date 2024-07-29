295c649f1ccc0192174e718a7b60f9fcb4f04afa
w <ghostoftheshell@mailbox.org>
2024-07-29 14:59:42 +0300

    Add test context alloc and free functions

--- test_case.c
+++ test_case.c
@@ -1,7 +1,35 @@
 #ifndef TEST_CASE_H_
 #define TEST_CASE_H_
-typedef struct {
-} TestContext;
+
+#include <stddef.h>
 
 typedef TestCase (*TestFunc) (TestContext);
+
+typedef struct {
+	// TODO: init?
+	// TODO: arraylist?
+	size_t testCount;
+	TestCase *tests;
+
+} TestContext;
+
+static inline TestContext
+tTC_AllocTestContext() {
+	TestContext tc;
+	tc.testCount = 0;
+	tc.tests = malloc(100 * sizeof(TestCase));
+	return tc;
+}
+
+static inline void
+tTC_AddTest(TestCase tc) {
+	tc->tests[testCount++] = tc;
+}
+
+static inline void
+tTC_FreeTestContext(TestContext *tc) {
+	free(tc->tests);
+	tc->tests = NULL;
+	tc->testCount = 0;
+}
 #endif
